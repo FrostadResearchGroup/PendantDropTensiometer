eb3ccc0692094f215d3652831698b45648f217b2
frostad-john <john.m.frostad@gmail.com>
2017-06-08 11:43:49 -0700

    Major modifications to file - not functional
    Started cleanup of structure and documentation.
    Added test code with flags.
    Added additional functions.
    Combined functions from redundant file (ylpsolver_fitting.py).

--- data_processing.py
+++ data_processing.py
@@ -7,20 +7,69 @@ Created on Wed May 17 18:26:21 2017
 """
 
 import numpy as np
-import scipy
+
+from scipy import optimize
+from scipy.integrate import ode
 import matplotlib.pyplot as plt
-#from numpy import loadtxt
-from ylpsolver_fitting import odesolver
 
 
-def workfile(params,deltaRho,xData,zData):
+#set equations
+def ode_system(t,y,bond):
+    phi, r, z = y
+    f = [2-bond*(z)-np.sin(phi)/(r), np.cos(phi), np.sin(phi)]
+    return f
+    
+#define ode solver function
+def young_laplace(Bo,nPoints,L):
+    """
+    Bo = float - Bond number
+    nPoints = int - number of integration points desired
+    L = float - final arc length for range of integration
+    """
+        
+    # set solver
+    solver = ode(ode_system)
+    solver.set_integrator('dopri5')
+
+    current_s=1
+    N=100
+
+    bond=params
+    
+    #sets bond number to value inputted when ylp_func is called by solver
+    solver.set_f_params(bond)
+
+    #set initial values
+    s0 = -current_s
+    y0 = [0.00001,0.00001,0.00001]
+    solver.set_initial_value(y0,s0)
+    
+    # Create the array `t` of time values at which to compute
+    # the solution, and create an array to hold the solution.
+    # Put the initial value in the solution array.
+    s1 = current_s
+    s = np.linspace(s0, s1, N)
+    sol = np.empty((N, 3))
+    sol[0] = y0
+    
+    # Repeatedly call the `integrate` method to advance the
+    # solution to time t[k], and save the solution in sol[k].
+    k = 1
+    while solver.successful() and solver.t < s1:
+        solver.integrate(s[k])
+        sol[k] = solver.y
+        k += 1
+        
+    return sol
+
+def objective_fun(params,deltaRho,xData,zData):
     gamma = params[0]
     R0 = params[1]
     #takes in initial bond number and optimizes through objective function as 
     #defined by N. Alvarez et. al
     bond= deltaRho*9.81*R0**2/gamma
     # print outputs of each section
-    model=odesolver(bond)
+    model=young_laplace(bond)
     
     xModel=model[:,1]
     zModel=model[:,2]
@@ -53,42 +102,74 @@ def workfile(params,deltaRho,xData,zData):
     rsq=np.min(output,axis=0)
     
     return np.sum(rsq)
+    
+def line(x,m,b):
+    y = m*x + b
+    return y
+    
+def test_obj_fun(params,x_data,y_data):
+    m = params[0]
+    b = params[1]
+    y_fit = line(x_data,m,b)
+    error = np.sum((y_fit-y_data)**2)
+    return error
 
 if __name__ == "__main__":
- 
-#### Acutal Data Points 
-    Bond_actual=1  
-    r0_actual=.8
-    deltaRho=900
-    sigmaActual=deltaRho*9.81*r0_actual**2/Bond_actual
     
+    testYLP = True
+    testLine = False
     
-    xActual=odesolver(Bond_actual)[:,1]
-    zActual=odesolver(Bond_actual)[:,2]
-    a
-    xActual_App=np.append(list(reversed(-xActual)),xActual[1:])
-    zActual_App=np.append(list(reversed(zActual)),zActual[1:])
-    ################################################################  
+    if testLine:
+        # Generate test data
+        N = 100
+        m_data = -2.321
+        b_data = 4.567
+        guess = [0,0]
+        x_data = np.linspace(0,10,N)
+        y_data = line(x_data,m_data,b_data)
+        
+        output = optimize.minimize(test_obj_fun,guess,args=(x_data,y_data),
+                                   method='Nelder-Mead')
+        m_fit = output.x[0]
+        b_fit = output.x[1]
+        
+        print m_fit, b_fit
     
-    sigmaGuess=5
-    R0Guess=.1
-    
-    initGuess=[sigmaGuess,R0Guess]
-    
-    r=scipy.optimize.minimize(workfile,initGuess,args=(deltaRho,xActual_App,
-                              zActual_App),method='Nelder-Mead')
-    
-    sigmaFinal=r.x[0]
-    R0Final=r.x[1]
-    Bond_final=deltaRho*9.81*r.x[1]**2/r.x[0]
-    
-    fitted=odesolver(Bond_final)
-    
-    xCurveFit=fitted[:,1]*R0Final
-    zCurveFit=fitted[:,2]*R0Final
-    xCurveFit_App=np.append(list(reversed(-xCurveFit)),xCurveFit[1:])
-    zCurveFit_App=np.append(list(reversed(zCurveFit)),zCurveFit[1:])
-    
-    
-    #plt.plot(xActual_App,zActual_App,'ro')
-    plt.plot(xCurveFit_App,zCurveFit_App,'bo')
+    if testYLP:
+    #### Acutal Data Points 
+        Bond_actual=1  
+        r0_actual=.8
+        deltaRho=900
+        sigmaActual=deltaRho*9.81*r0_actual**2/Bond_actual
+        
+        temp = young_laplace(Bond_actual)
+        xActual = temp[:,1]
+        zActual = temp[:,2]
+        
+        xActual_App=np.append(list(reversed(-xActual)),xActual[1:])
+        zActual_App=np.append(list(reversed(zActual)),zActual[1:])
+        ################################################################  
+        
+#        sigmaGuess=5
+#        R0Guess=.1
+#        
+#        initGuess=[sigmaGuess,R0Guess]
+#        
+#        r=optimize.minimize(objective_fun,initGuess,args=(deltaRho,xActual_App,
+#                                  zActual_App),method='Nelder-Mead')
+#        
+#        sigmaFinal=r.x[0]
+#        R0Final=r.x[1]
+#        Bond_final=deltaRho*9.81*r.x[1]**2/r.x[0]
+#        
+#        fitted=young_laplace(Bond_final)
+#        
+#        xCurveFit=fitted[:,1]*R0Final
+#        zCurveFit=fitted[:,2]*R0Final
+#        xCurveFit_App=np.append(list(reversed(-xCurveFit)),xCurveFit[1:])
+#        zCurveFit_App=np.append(list(reversed(zCurveFit)),zCurveFit[1:])
+        
+        
+        plt.plot(xActual_App,zActual_App,'ro')
+        plt.axis('equal')
+#        plt.plot(xCurveFit_App,zCurveFit_App,'bo')
