919b01e04fd851574dd164c7bc1de91268ea2dc0
FrostadResearch <frostadresearch@gmail.com>
2017-06-14 07:01:29 -0700

    Update data_processing.py
    - Created flag for objective function to investigate tiling of arrays, format of matrices, and other calculation procedures.
    - Added short, descriptor comments to each of the functions.

--- data_processing.py
+++ data_processing.py
@@ -15,6 +15,11 @@ import matplotlib.pyplot as plt
 
 #set equations
 def ode_system(t,y,bond):
+    """
+    Outputs system of ordinary differential equations with non-dimensionalized
+    coordinates. 
+    f=[d(fi)/ds , d(X)/ds , d(Z)/ds]
+    """
     phi, r, z = y
     f = [2-bond*(z)-np.sin(phi)/(r), np.cos(phi), np.sin(phi)]
     return f
@@ -62,38 +67,43 @@ def young_laplace(Bo,nPoints,L):
         
     return sol
 
-def objective_fun(params,deltaRho,xCoords,zCoords):
-    gamma = params[0]
-    R0 = params[1]
-    #takes in initial bond number and optimizes through objective function as 
-    #defined by N. Alvarez et. al
-    bond= deltaRho*9.81*R0**2/gamma
-    # print outputs of each section
-    model=young_laplace(bond,400,2)
+def objective_fun(params,deltaRho,xData,zData):
+    """
+    Takes in initial bond number and optimizes through objective function as 
+    defined by N. Alvarez et. al
     
-    xModel=model[:,1]*R0
-    zModel=model[:,2]*R0
+    Fitting parameters are Bond number and radius of curvature at apex.
+    """
+    gamma = params[0]
+    apexRadius = params[1]
+
+    bond= deltaRho*9.81*apexRadius**2/gamma
+    # print outputs of each section
+    model=young_laplace(bond,400,1)
+    
+    xModel=model[:,1]*apexRadius
+    zModel=model[:,2]*apexRadius
     xModel_App=np.append(list(reversed(-xModel)),xModel[1:])
     zModel_App=np.append(list(reversed(zModel)),zModel[1:])
-     
+    
+    #sizing matrices
     xModelFit=xModel_App[:-1]
     zModelFit=zModel_App[:-1]
     xModelFit_plus=xModel_App[1:]
     zModelFit_plus=zModel_App[1:]
     
-    xData=xCoords
-    zData=zCoords
+    #creating grid of real and model values
+    xActualgrid=np.array([xData,]*len(xModelFit))
+    zActualgrid=np.array([zData,]*len(zModelFit))
     
-    xdatagrid=np.tile(xData,(len(xModelFit),1))
-    zActualgrid=np.tile(zData,(len(zModelFit),1))
+    xFit=np.array([xModelFit,]*len(xData)).transpose()
+    zFit=np.array([zModelFit,]*len(zData)).transpose()
+    xFit_plus=np.array([xModelFit_plus,]*len(xData)).transpose()
+    zFit_plus=np.array([zModelFit_plus,]*len(zData)).transpose()
     
-    xFit=np.tile(xModelFit,(len(xData),1)).transpose()
-    zFit=np.tile(zModelFit,(len(zData),1)).transpose()
-    xFit_plus=np.tile(xModelFit_plus,(len(xData),1)).transpose()
-    zFit_plus=np.tile(zModelFit_plus,(len(zData),1)).transpose()
-    
+    #outputting r-squared     
     a=(zFit-zActualgrid)*(xFit-xFit_plus)
-    b=(xFit-xdatagrid)*(zFit-zFit_plus)
+    b=(xFit-xActualgrid)*(zFit-zFit_plus)
     c=np.power((xFit-xFit_plus),2)
     d=np.power((zFit-zFit_plus),2)
     
@@ -104,10 +114,17 @@ def objective_fun(params,deltaRho,xCoords,zCoords):
     return np.sum(rsq)
     
 def line(x,m,b):
+    """
+    Outputs f(x) using the standard slope, y-intercept, and x 
+    """
     y = m*x + b
     return y
     
-def test_obj_fun(params,x_data,y_data):
+def test_fit(params,x_data,y_data):
+    """
+    Calculates the sum of the residual error of f(x) data, fits to slope and
+    y-intercept
+    """
     m = params[0]
     b = params[1]
     y_fit = line(x_data,m,b)
@@ -119,7 +136,8 @@ if __name__ == "__main__":
     plt.close('all')
     
     testYLP = False
-    testLine = True
+    testLine = False
+    testObjFun = True
     
     if testLine:
         # Generate test data
@@ -130,7 +148,7 @@ if __name__ == "__main__":
         x_data = np.linspace(0,10,N)
         y_data = line(x_data,m_data,b_data)
         
-        output = optimize.minimize(test_obj_fun,guess,args=(x_data,y_data),
+        output = optimize.minimize(test_fit,guess,args=(x_data,y_data),
                                    method='Nelder-Mead')
         m_fit = output.x[0]
         b_fit = output.x[1]
@@ -138,9 +156,10 @@ if __name__ == "__main__":
         print m_fit, b_fit
     
     if testYLP:
+
     #### Acutal Data Points 
-        sigmaActual=0.073
-        r0_actual=.003
+        sigmaActual=0.05
+        r0_actual=.005
         deltaRho=900
         Bond_actual=deltaRho*9.81*r0_actual**2/sigmaActual
         
@@ -150,11 +169,11 @@ if __name__ == "__main__":
         
         xActual_App=np.append(list(reversed(-xActual)),xActual[1:])
         zActual_App=np.append(list(reversed(zActual)),zActual[1:])
-        ################################################################  
-        
-        sigmaGuess=.05
-        R0Guess=.002
-        
+  
+        ###########
+        sigmaGuess=.75*sigmaActual
+        R0Guess=.75*r0_actual
+    
         initGuess=[sigmaGuess,R0Guess]
         
         for i in range(3):
@@ -179,3 +198,56 @@ if __name__ == "__main__":
             plt.axis('equal')
             plt.plot(xCurveFit_App,zCurveFit_App,'b')
             
+    if testObjFun:
+        sigmaActual=0.08
+        r0_actual=.005
+        deltaRho=900
+        Bond_actual=deltaRho*9.81*r0_actual**2/sigmaActual
+        
+        temp = young_laplace(Bond_actual,50,1)
+        xActual = temp[:,1]*r0_actual
+        zActual = temp[:,2]*r0_actual
+        
+        xData=np.append(list(reversed(-xActual)),xActual[1:])
+        zData=np.append(list(reversed(zActual)),zActual[1:])
+  
+        ###########
+        sigmaGuess=2*sigmaActual
+        R0Guess=1.25*r0_actual
+    
+        initGuess=[sigmaGuess,R0Guess]
+        
+        gamma = initGuess[0]
+        r0 = initGuess[1]
+        #takes in initial bond number and optimizes through objective function as 
+        #defined by N. Alvarez et. al
+        bond= deltaRho*9.81*r0**2/gamma
+        # print outputs of each section
+        model=young_laplace(bond,400,1)
+        
+        xModel=model[:,1]*r0
+        zModel=model[:,2]*r0
+        xModel_App=np.append(list(reversed(-xModel)),xModel[1:])
+        zModel_App=np.append(list(reversed(zModel)),zModel[1:])
+         
+        xModelFit=xModel_App[:-1]
+        zModelFit=zModel_App[:-1]
+        xModelFit_plus=xModel_App[1:]
+        zModelFit_plus=zModel_App[1:]
+        
+        xActualgrid=np.array([xData,]*len(xModelFit))
+        zActualgrid=np.array([zData,]*len(zModelFit))
+        
+        xFit=np.array([xModelFit,]*len(xData)).transpose()
+        zFit=np.array([zModelFit,]*len(zData)).transpose()
+        xFit_plus=np.array([xModelFit_plus,]*len(xData)).transpose()
+        zFit_plus=np.array([zModelFit_plus,]*len(zData)).transpose()
+        
+        a=(zFit-zActualgrid)*(xFit-xFit_plus)
+        b=(xFit-xActualgrid)*(zFit-zFit_plus)
+        c=np.power((xFit-xFit_plus),2)
+        d=np.power((zFit-zFit_plus),2)
+        
+        output=np.power(((a-b)/np.power((c+d),0.5)),2)
+        
+        rsq=np.min(output,axis=0)
