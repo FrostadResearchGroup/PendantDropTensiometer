1e4ed5459ef20f667543886e1e5fe0606319d1ed
Yohan Wiputra <yowiputra@gmail.com>
2017-06-19 12:31:00 -0700

    Incorporated NCSUARC's IDS Python Module for image capture
    Removed non-functional elements for now

--- image_extraction.py
+++ image_extraction.py
@@ -4,15 +4,8 @@ Created on Tue Jun 13 11:08:31 2017
 
 @author: Yohan
 """
-
-from PyQt4.QtGui import QFileDialog
 import cv2
-import matplotlib.image as mpimg
-import numpy as  np
-import ctypes
-
-#change the file path accordingly or have the dll file in the same folder
-uEyeDll = ctypes.cdll.LoadLibrary("ueye_api_64.dll")
+import ids
 
 def load_image_file(filePath):
     """
@@ -34,58 +27,52 @@ def get_new_image():
     """
     use the uEye camera to take picture
     """
-    #connect camera
-    cam = ctypes.c_uint32(0)
-    hWnd = ctypes.c_voidp()
-    msg=uEyeDll.is_InitCamera(ctypes.byref(cam),hWnd)
-    ErrChk=uEyeDll.is_EnableAutoExit (cam, ctypes.c_uint(1))
-    if ~ErrChk:
-        print (' Camera Connected')
-    IS_CM_SENSOR_RAW8  =ctypes.c_int(11)
-    nRet = uEyeDll.is_SetColorMode(cam,IS_CM_SENSOR_RAW8)
-    IS_SET_TRIGGER_SOFTWARE = ctypes.c_uint(0x1000)
-    nRet = uEyeDll.is_SetExternalTrigger(cam, IS_SET_TRIGGER_SOFTWARE)
+    cam = ids.Camera()
+    cam.color_mode = ids.ids_core.COLOR_BGR8    # Get images in BGR format for JPEG compatibility
+    cam.exposure = 5                            # Set initial exposure to 5ms
+    cam.auto_exposure = True
+    cam.continuous_capture = True               # Start image capture
     
-    #allocate memory
-    width_py = 1600
-    height_py = 1200
-    pixels_py =8
+    #get image
+    meta = cam.next_save("testpic.jpg")
+    return
     
-    width = ctypes.c_int(width_py) #convert python values into c++ integers
-    height = ctypes.c_int(height_py) 
-    bitspixel=ctypes.c_int(pixels_py)
-    pcImgMem = ctypes.c_char_p() #create placeholder for image memory
-    pid=ctypes.c_int()
+#For Testing Purposes    
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
     
-    ErrChk=uEyeDll.is_AllocImageMem(cam, width, height,  bitspixel, ctypes.byref(pcImgMem), ctypes.byref(pid))
-    if ~ErrChk:
-        print (' Success')
-    else:
-        print (' Memory allocation failed, no camera with value' +str(cam.value))
+    #test flags: change to True when testing specific functions
+    flag1 = False
+    flag2 = False
+    flag3 = False
+    flag4 = False
+    flag5 = False
+    flag6 = False
+    
+    #flag1 = test for load_image_file()
+    if(flag1 == True):
+        imagePath = "H20 in PDMS.jpg"
+        image = load_image_file(imagePath)
+        plt.imshow(image, cmap = "gray")
         
-    # Get image data    
-    uEyeDll.is_SetImageMem(cam, pcImgMem, pid)
-    ImageData = np.ones((height_py,width_py),dtype=np.uint8)
-    uEyeDll.is_FreezeVideo (cam, ctypes.c_int(0x0000))  #IS_DONT_WAIT  = 0x0000, or IS_GET_LIVE = 0x8000
-    uEyeDll.is_CopyImageMem (cam, pcImgMem, pid, ImageData.ctypes.data)
-    return ImageData
-    
-def get_new_video():
-    
-    
-def preview_camera_view(mplwidget, item, idCode):
-    """
-    previews the image or video. Takes in an idCode: 0 for image, 1 for video
-    """
-    if(idCode == 0):
-        mplwidget.axes.imshow(item, cmap ='gray')
-        mplwidget.figure.canvas.draw()
-    else if(idCode == 1):
+    #flag2 = test for load_video_file()
+    if (flag2 == True):
+        videoPath = "SampleVideo_1280x720.mp4"
+        cap = load_video_file(videoPath)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
         
-    else:
-        return None
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-def analyze_frame():
-    
-    
-    
+            cv2.imshow('frame',gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        
+    #flag3 = test for get_new_image()
+    if(flag3 == True):
+        get_new_image()
+        
+        
