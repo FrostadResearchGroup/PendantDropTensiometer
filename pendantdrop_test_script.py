7db3aae05fc81801a912c67aed87c53d4d76e8ce
abnerbog <abnerbog@buffalo.edu>
2017-07-07 14:01:41 -0700

    Second Functional Version
    - Removed unused functions from script
    - Added comments where necessary
    - Cleaned up formatting

--- pendantdrop_test_script.py
+++ pendantdrop_test_script.py
@@ -15,10 +15,14 @@ import tkFileDialog
 import matplotlib.pyplot as plt
 import os
 
-#INput Parameters  
+##############################################################################
+
+#input parameters  
 deltaRho = 998
 actualDiameter = 2.108 #in mm
 reloads = 1
+
+##############################################################################
 
 #function to display image on plt
 def display_plot(obj):
@@ -31,9 +35,6 @@ def display_scat(obj):
     plt.scatter(obj[:,0],obj[:,1])
     plt.pause(1)
     
-"""
-image extraction
-"""
 #get image file path
 filePath = tkFileDialog.askopenfilename()
 
@@ -45,9 +46,6 @@ if os.path.isfile(filePath):
 else:
     print("file not loaded!")
     
-"""
-image processing
-"""
 #binarize image
 binarizedImage = ip.binarize_image(image)
 display_plot(binarizedImage)
@@ -65,19 +63,14 @@ plt.scatter(interfaceCoordinates[:,0],interfaceCoordinates[:,1])
 plt.pause(1)
 print("boundary coordinates acquired")
 
-#get rotation angle
-rotationAngle = ip.get_rotation_angle(interfaceCoordinates)
-rotationAngle = 0
-print ("rotation angle against vertical axis is " + str(rotationAngle))
-
 #get magnification ratio
-magnificationRatio = ip.get_magnification_ratio(interfaceCoordinates, actualDiameter)
+magnificationRatio = ip.get_magnification_ratio(interfaceCoordinates,actualDiameter)
 print ("magnification ratio is " + str(magnificationRatio))
 
 #isolate drop
 pts = plt.ginput(1)
 x,y = pts[0]
-xCoords = [min(interfaceCoordinates[:,0]),max(interfaceCoordinates[:,0])] # map applies the function passed as 
+xCoords = [min(interfaceCoordinates[:,0]),max(interfaceCoordinates[:,0])] 
 yCoords = [y,y]
 dropCoords = ip.isolate_drop(xCoords,yCoords,interfaceCoordinates)
 print ("drop isolated")
@@ -85,28 +78,22 @@ print ("drop isolated")
 #shift coordinates so apex is at 0,0
 plt.gca().invert_yaxis()
 newCenter = [0,0]
-
 shiftedCoords = ip.shift_coords(dropCoords[:,0],dropCoords[:,1],newCenter)
 display_scat(shiftedCoords)
 print ("shifted apex to 0,0")
 
-#rotate drop
-rotatedCoords = ip.rotate_coords(shiftedCoords ,rotationAngle*-1,'degrees')
-display_scat(rotatedCoords)
-print("corrected angle")
-
 #scale drop
-scaledCoords = ip.scale_drop(rotatedCoords,magnificationRatio)
-display_scat(scaledCoords)
+scaledCoords = ip.scale_drop(shiftedCoords,magnificationRatio)
 print ("scaled coordinates according to magnification ratio")
 
 #reorder data points
 xData,zData = ip.reorder_data(scaledCoords)
-
 s,xe,apexRadiusGuess = dp.bond_calc(xData,zData)
 bondInit = dp.s_interp(s,xe)
 
+#run through optimization routine
 surfTen,apexRadius,thetaRotation,bondNumber = dp.optimize_params(xData,zData,bondInit,
                                                 apexRadiusGuess,deltaRho,reloads)
 
+#output surface tension
 print "Surface tension = %.4g mN/m" %(surfTen*10**3)
